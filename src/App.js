a364a06c0074138d03cd7ccb8b057bfc4a35f1f2
ziwei531 <59821731+ziwei531@users.noreply.github.com>
2021-09-09 23:43:34 +0800

    added a console log msg

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Lower from './components/lower-part';
 import Input from './components/input';
 
 function App() {
+  console.log("%cThanks for using this application :)","color: black; font-size: 50px; background-color: #d9cad9;");
+  console.log("%cGithub: https://github.com/ziwei531","color: black; font-size: 20px; background-color: #d9cad9;");
+  
   return (
     <>
       <Upper />
